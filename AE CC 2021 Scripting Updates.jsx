b5f64325270c61d4c5733789c15be1a03fe57e07
Nate Lovell <Natelovell99@gmail.com>
2021-04-18 02:35:16 -0600

    Create AE CC 2021 Scripting Updates.jsx

---
+++ AE CC 2021 Scripting Updates.jsx
@@ -0,0 +1,10 @@
+﻿// https://after-effects-scripting-guide.readthedocs.io/_/downloads/en/latest/pdf/
+
+AVItem.isMediaReplacementCompatible;
+AVLayer.addToMotionGraphicsTemplate(comp);
+AVLayer.addToMotionGraphicsTemplateAs(comp, name);
+AVLayer.canAddToMotionGraphicsTemplate(comp);
+
+// Property.alternateSource
+// Property.canSetAlternateSource
+// Property.setAlternateSource()
